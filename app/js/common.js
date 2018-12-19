5955e082bc719a2e1d395153f626f6f6ee33a053
Roman Chuklinov <xjtvu85@gmail.com>
2018-12-19 23:22:24 +1000

    Запущены табы

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,16 @@
 $(function() {
-	// Custom JS
+
+	$('.js-tab-trigger').click(function () { 
+		var id = $(this).attr('data-tab'); // присваиваем в id значение дата-атрибута залоговков ТАБов
+		content = $('.js-tab-content[data-tab="' + id + '"]');
+		
+		$('.js-tab-trigger.active').removeClass('active'); //убираем класс Active у активной вкладки
+		$(this).addClass('active'); //добавляем класс active только у таба, по которому кликнули - this
+		
+		$('.js-tab-content.active').removeClass('active'); //убираем класс Active у всех content
+		content.addClass('active'); //добавляем класс active только у контента, по табу которому кликнули -- content
+
+		
+	});
 
 });
